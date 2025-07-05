035ce432bf7bda1319339a8af554dda3a54837ca
ABHRCC <info@abhrc.org>
2025-07-05 21:35:32 +0300

    Delete file

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 
-export default function Home() {
-  return (
-    <div style={{ padding: 50, fontFamily: 'Arial' }}>
-      <h1>Welcome to ABHRC Test Site</h1>
-      <p>This is a placeholder Next.js site deployed on Vercel.</p>
-    </div>
-  );
-}
